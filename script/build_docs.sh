0806e91fa7a87cefdd4a0d2af259ea5fcb44fb1e
rjfovo <rjf12138@gmail.com>
2024-12-22 22:36:33 +0800

    增加文档初始化

--- script/build_docs.sh
+++ script/build_docs.sh
@@ -4,6 +4,6 @@ script_path=`pwd`
 script_path=${script_path}/../cutefish
 cd ${script_path}/code/docs
 
-sphinx-build -M html ./source/ /var/www/myos/
+/root/.local/pipx/venvs/sphinx-rtd-theme/bin/sphinx-build -M html ./source/ /var/www/myos/
 
 exit 0
